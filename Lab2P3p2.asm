6d4125a1f8934a2200dc9c1507e2c42e0f9b730e
916-Sampalean-Sorin <sampaleansorin@outlook.com>
2023-11-01 13:33:03 +0200

    am inceputP3p2

--- Lab2P3p2.asm
+++ Lab2P3p2.asm
@@ -11,12 +11,23 @@ import exit msvcrt.dll    ; exit is a function that ends the calling process. It
 ; our data is declared here (the variables needed by our program)
 segment data use32 class=data
     ; ...
+    a db 2
+    b db 4
+    c db 5
+    d db 7
+    e dw 9000
+    f dw 1777
+    g dw 14
+    h dw 15
 
 ; our code starts here
 segment code use32 class=code
     start:
         ; ...
-    
+        mov ax, word[f] ;word f in ax
+        mov bx, word[e] ;word e in bx
+        sub bx, 2       ;(e-2) -> bx
+        mul bx          ;f*(e-2) -> eax
         ; exit(0)
         push    dword 0      ; push the parameter for exit onto the stack
         call    [exit]       ; call exit to terminate the program
