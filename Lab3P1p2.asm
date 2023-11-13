ba1b2e13b9cad362e9affac2ccbb15728727c1d8
916-Sampalean-Sorin <sampaleansorin@outlook.com>
2023-11-13 16:20:06 +0200

    Finished P1p2

--- Lab3P1p2.asm
+++ Lab3P1p2.asm
@@ -10,12 +10,34 @@ import exit msvcrt.dll    ; exit is a function that ends the calling process. It
 
 ; our data is declared here (the variables needed by our program)
 segment data use32 class=data
-    ; ...
-
+    ; a - byte, b - word, c - double word, d - qword - Signed representation
+    a db 31
+    b dw 12
+    c dd 345
+    d dq 1989
 ; our code starts here
 segment code use32 class=code
     start:
-        ; ...
+        ; c+a+b+b+a
+    mov ebx, dword[c] ;ebx = c
+    xor eax, eax      ;eax=0
+    mov al,byte[a]    ;al = a
+    cbw               ;al->ax
+    cwde              ;ax->eax
+    add eax,ebx       ;eax = c+a
+    mov ebx,eax       ;ebx = c+a
+    xor eax,eax       ;eax = 0
+    mov ax, word[b]   ;ax = b
+    cwde              ;ax -> eax
+    add ebx,eax       ;ebx = c+a+b
+    add ebx,eax       ;ebx = c+a+b+b
+    xor eax, eax      ;eax=0
+    mov al,byte[a]    ;al = a
+    cbw               ;al->ax
+    cwde              ;ax->eax
+    add ebx,eax       ;ebx = c+a+b+b+a - reazolvarea
+    
+    
     
         ; exit(0)
         push    dword 0      ; push the parameter for exit onto the stack
