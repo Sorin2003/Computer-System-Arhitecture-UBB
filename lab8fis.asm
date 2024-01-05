4e015f231eaff030e3654104ff7d2e82bebf9c2e
Sampalean Sorin <sampaleansorin@outlook.com>
2024-01-05 20:14:39 +0200

    update

--- lab8fis.asm
+++ lab8fis.asm
@@ -4,24 +4,29 @@ bits 32 ; assembling for the 32 bits architecture
 global start        
 
 ; declare external functions needed by our program
-extern exit               ; tell nasm that exit exists even if we won't be defining it
+extern exit, fopen, fprintf, fclose               ; tell nasm that exit exists even if we won't be defining it
 import exit msvcrt.dll    ; exit is a function that ends the calling process. It is defined in msvcrt.dll
                           ; msvcrt.dll contains exit, printf and all the other important C-runtime specific functions
-
+import fopen msvcrt.dll
+import fprintf msvcrt.dll
+import fclose msvcrt.dll
 ; our data is declared here (the variables needed by our program)
 segment data use32 class=data
     ;A file name and a text (defined in the data segment) are given. The text contains lowercase letters, uppercase letters, digits and special characters. Replace all the special characters from the given text with the character 'X'. Create a ;file with the given name and write the generated text to file.
     ; ...
-   text dw "Asta este % balta $",0
-   ;text dw "da  merge *acest %cod &", 0
+   ;text dw "Asta este % balta $",0
+   text dw "da  merge *acest %cod &", 0
+   ;text dw "da ", 0
    len equ $-text-3
    write times len dw 0
+   filename db 'output.txt', 0
+   mode     db 'w', 0
+   format   db 's', 0
 
 ; our code starts here
 segment code use32 class=code
     start:
         ; ...
-        cmp esi, len
         mov cl, 'X'
         xor esi, esi
         read:
@@ -31,31 +36,48 @@ segment code use32 class=code
             jae verifyc              ;if it is greater or equal to 'A' verify if it is a capital letter or smth else
         
     special:                         ;if the code jumps here, then we write an X instead of the caracter
-        mov [write + esi], cl       
+        mov [write + esi], cl        ;put the special caracter into the res 
         inc esi
         cmp esi, len
         jb read
-        jae exit
-    verifyc:
+        jae file
+    verifyc:                        ;verify if it is capital letter else verify if it is small letter
         cmp al, 'Z'
         jbe writel
         ja verifyl
-    verifyl:
+    verifyl:                        ;verify if it is small letter
         cmp al, 'a'
         jb special
         jae verifyli
     
-    verifyli:
+    verifyli:                       ;verify if it is small letter else write an x instead the special caracter
         cmp al,'z'
         jbe writel
         ja special
         
-    writel:
+    writel:                         ;write letters
         mov [write + esi], al
             inc esi
             cmp esi, len
             jb read
-            jae exit
+            jae file
+     
+    file:
+        push dword mode
+        push dword filename
+        call [fopen]
+        add esp, 8      ;open
+        mov ebx, eax
+
+        push dword write
+        ;push dword format
+        push ebx
+        call [fprintf]
+        add esp, 8      ;write
+        
+        push ebx
+        call [fclose]
+        add esp, 4      ;close
         ; exit(0)
         push    dword 0      ; push the parameter for exit onto the stack
         call    [exit]       ; call exit to terminate the program
